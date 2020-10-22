932c43623a902511950dae25a4f84a834387ab1c
Tsuyoshi Ishida <tishida@ioa.s.u-tokyo.ac.jp>
2020-10-22 19:11:43 +0900

    Lint by flake8 and black

--- scripts/main.py
+++ scripts/main.py
@@ -1,38 +1,39 @@
-##### dependencies #####
+# dependencies
 import sys
-import pathlib, shutil
-import numpy as np
+import pathlib
+import shutil
 import yaml
-import time, datetime
-from astropy import units as u
+import time
+import datetime
+
 from astropy.io import fits
 from subprocess import Popen, PIPE
-from utils import functions as f
+from .utils import functions as f
 
 
-##### get current time #####
-now   = datetime.datetime.now()
+# get current time
+now = datetime.datetime.now()
 start = time.time()
-anaid = now.strftime('%Y%m%d_%H%M%S')
+anaid = now.strftime("%Y%m%d_%H%M%S")
 
-##### get arguments #####
+# get arguments
 argv = sys.argv
 argc = len(argv)
 if argc != 2 and argc != 3:
-    raise SyntaxError('The number of arguments is wrong!')
-if '-qlook' in argv:
+    raise SyntaxError("The number of arguments is wrong!")
+if "-qlook" in argv:
     qlook = True
-    argv.remove('-qlook')
+    argv.remove("-qlook")
 else:
     qlook = False
 ymlname = argv[-1]
 
-##### directory setting #####
-cur_dir        = pathlib.Path.cwd()
+# directory setting
+cur_dir = pathlib.Path.cwd()
 cloverleaf_dir = pathlib.Path(__file__).parents[1].resolve()
-data_dir       = cloverleaf_dir / 'data'
-input_dir      = cloverleaf_dir / 'input' / anaid
-results_dir     = cloverleaf_dir / 'results' / anaid
+data_dir = cloverleaf_dir / "data"
+input_dir = cloverleaf_dir / "input" / anaid
+results_dir = cloverleaf_dir / "results" / anaid
 
 input_dir.mkdir()
 results_dir.mkdir()
@@ -41,123 +42,161 @@ with open(ymlname) as file:
     yml = yaml.load(file)
 shutil.copy(ymlname, input_dir)
 
-##### filenames #####
-prefix    = yml['primary_params']['prefix']
-fitsfile  = data_dir / pathlib.Path(yml['filename']['obsfits'])
-noisefile = data_dir / pathlib.Path(yml['filename']['noisefits'])
-psffile   = data_dir / pathlib.Path(yml['filename']['psffits'])
+# filenames
+prefix = yml["primary_params"]["prefix"]
+fitsfile = data_dir / pathlib.Path(yml["filename"]["obsfits"])
+noisefile = data_dir / pathlib.Path(yml["filename"]["noisefits"])
+psffile = data_dir / pathlib.Path(yml["filename"]["psffits"])
 
-mask  = yml['user_params']['mask']
-prior = yml['user_params']['prior']
-mcmc  = yml['user_params']['mcmc']
+mask = yml["user_params"]["mask"]
+prior = yml["user_params"]["prior"]
+mcmc = yml["user_params"]["mcmc"]
 if mask:
-    maskfile = data_dir / pathlib.Path(yml['filename']['maskfits'])
+    maskfile = data_dir / pathlib.Path(yml["filename"]["maskfits"])
     shutil.copy(maskfile, input_dir)
 else:
-    maskfile = ''
+    maskfile = ""
 if prior:
-    priorfile = data_dir / pathlib.Path(yml['filename']['priorfile'])
+    priorfile = data_dir / pathlib.Path(yml["filename"]["priorfile"])
     shutil.copy(priorfile, input_dir)
 else:
-    priorfile = ''
+    priorfile = ""
 
-input_b   = input_dir / pathlib.Path(prefix + yml['filename']['input_b'])
-input_a   = input_dir / pathlib.Path(prefix + yml['filename']['input_a'])
-input_as  = input_dir / pathlib.Path(prefix + yml['filename']['input_as'])
-optfile   = results_dir / pathlib.Path(prefix + yml['filename']['optfile'])
-imgfits   = results_dir / pathlib.Path(prefix + yml['filename']['imgfits'])
-srcfits   = results_dir / pathlib.Path(prefix + yml['filename']['srcfits'])
-resfits   = results_dir / pathlib.Path(prefix + yml['filename']['resfits'])
-imgout    = results_dir/ pathlib.Path(yml['filename']['imgout'])
-statout   = results_dir/ pathlib.Path(yml['filename']['statout'])
+input_b = input_dir / pathlib.Path(prefix + yml["filename"]["input_b"])
+input_a = input_dir / pathlib.Path(prefix + yml["filename"]["input_a"])
+input_as = input_dir / pathlib.Path(prefix + yml["filename"]["input_as"])
+optfile = results_dir / pathlib.Path(prefix + yml["filename"]["optfile"])
+imgfits = results_dir / pathlib.Path(prefix + yml["filename"]["imgfits"])
+srcfits = results_dir / pathlib.Path(prefix + yml["filename"]["srcfits"])
+resfits = results_dir / pathlib.Path(prefix + yml["filename"]["resfits"])
+imgout = results_dir / pathlib.Path(yml["filename"]["imgout"])
+statout = results_dir / pathlib.Path(yml["filename"]["statout"])
 
-glafic_path = pathlib.Path(yml['path']['glafic'])
+glafic_path = pathlib.Path(yml["path"]["glafic"])
 
-##### basic source information #####
-zs        = yml['user_params']['zs']
-pix_ext_s = yml['user_params']['pix_ext_s']
+# basic source information
+zs = yml["user_params"]["zs"]
+pix_ext_s = yml["user_params"]["pix_ext_s"]
 
-##### model info #####
-primary_params   = yml['primary_params']
-secondary_params = yml['secondary_params']
-lens_models      = yml['model']['lens'].split('\n')
-source_models    = yml['model']['source'].split('\n')
-lens_num         = len(lens_models)
-source_num       = len(source_models)
-lens_opt         = yml['optimization']['lens'].split('\n')
-source_opt       = yml['optimization']['source'].split('\n')
+# model info
+primary_params = yml["primary_params"]
+secondary_params = yml["secondary_params"]
+lens_models = yml["model"]["lens"].split("\n")
+source_models = yml["model"]["source"].split("\n")
+lens_num = len(lens_models)
+source_num = len(source_models)
+lens_opt = yml["optimization"]["lens"].split("\n")
+source_opt = yml["optimization"]["source"].split("\n")
 
-##### main #####
-### make input_b ###
-f.makeinput(input_b, primary_params, secondary_params, lens_models, source_models, lens_opt, source_opt)
+# main
+# make input_b
+f.makeinput(
+    input_b,
+    primary_params,
+    secondary_params,
+    lens_models,
+    source_models,
+    lens_opt,
+    source_opt,
+)
 
-### optimization ###
+# optimization
 proc = Popen([glafic_path, input_b], stdin=PIPE)
-proc.communicate('readobs_extend {} {}\nreadnoise_extend {}\nreadpsf {}\nparprior {}\noptimize\nquit\n'\
-                .format(fitsfile, maskfile, noisefile, psffile, priorfile).encode())
+proc.communicate(
+    "readobs_extend {} {}\nreadnoise_extend {}\nreadpsf {}\nparprior {}\noptimize\nquit\n".format(
+        fitsfile, maskfile, noisefile, psffile, priorfile
+    ).encode()
+)
 shutil.move(cur_dir / optfile.name, optfile)
 
-### read fitting results ###
-with open(optfile, 'r') as fp_opt:
-    optdata         = fp_opt.readlines()
-    lens_models_a   = optdata[-source_num-lens_num-1:-source_num-1]
-    source_models_a = optdata[-source_num-1:-1]
+# read fitting results
+with open(optfile, "r") as fp_opt:
+    optdata = fp_opt.readlines()
+    lens_models_a = optdata[-source_num - lens_num - 1 : -source_num - 1]
+    source_models_a = optdata[-source_num - 1 : -1]
 
-    statdata = optdata[-source_num-lens_num-1-8].split()
-    chi2     = float(statdata[2])
-    ndata    = int(statdata[-1].rstrip(']'))
+    statdata = optdata[-source_num - lens_num - 1 - 8].split()
+    chi2 = float(statdata[2])
+    ndata = int(statdata[-1].rstrip("]"))
 
-### make input_a ###
-f.makeinput(input_a, primary_params, secondary_params, lens_models_a, source_models_a, lens_opt, source_opt)
+# make input_a
+f.makeinput(
+    input_a,
+    primary_params,
+    secondary_params,
+    lens_models_a,
+    source_models_a,
+    lens_opt,
+    source_opt,
+)
 
-### calculate modeled image ###
+# calculate modeled image
 proc = Popen([glafic_path, input_a], stdin=PIPE)
-proc.communicate('readpsf {}\nwriteimage 0 0\nquit\n'.format(psffile).encode())
+proc.communicate("readpsf {}\nwriteimage 0 0\nquit\n".format(psffile).encode())
 shutil.move(cur_dir / imgfits.name, imgfits)
 
-### mcmc ###
+# mcmc
 if mcmc:
-    mcmcfile  = results_dir / pathlib.Path(prefix + yml['filename']['mcmcfile'])
-    sigmafile = data_dir / pathlib.Path(yml['filename']['sigmafile'])
-    mcmc_n    = yml['user_params']['mcmc_n']
+    mcmcfile = results_dir / pathlib.Path(prefix + yml["filename"]["mcmcfile"])
+    sigmafile = data_dir / pathlib.Path(yml["filename"]["sigmafile"])
+    mcmc_n = yml["user_params"]["mcmc_n"]
     shutil.copy(sigmafile, input_dir)
 
     proc = Popen([glafic_path, input_a], stdin=PIPE)
-    proc.communicate('readobs_extend {} {}\nreadnoise_extend {}\nreadpsf{}\nparprior {}\nmcmc_sigma {}\nmcmc {} extend\nquit\n'\
-                    .format(fitsfile, maskfile, noisefile, psffile, priorfile, sigmafile, mcmc_n).encode())
+    proc.communicate(
+        "readobs_extend {} {}\nreadnoise_extend {}\nreadpsf{}\nparprior {}\nmcmc_sigma {}\nmcmc {} extend\nquit\n".format(
+            fitsfile, maskfile, noisefile, psffile, priorfile, sigmafile, mcmc_n
+        ).encode()
+    )
     shutil.move(cur_dir / mcmcfile.name, mcmcfile)
 
-### make input_as ###
-primary_params['pix_ext'] = pix_ext_s
-f.makeinput(input_as, primary_params, secondary_params, lens_models_a, source_models_a, lens_opt, source_opt)
+# make input_as
+primary_params["pix_ext"] = pix_ext_s
+f.makeinput(
+    input_as,
+    primary_params,
+    secondary_params,
+    lens_models_a,
+    source_models_a,
+    lens_opt,
+    source_opt,
+)
 
-### output the source plane ###
+# output the source plane
 proc = Popen([glafic_path, input_as], stdin=PIPE)
-proc.communicate('writeimage_ori 0 0\nquit\n'.encode())
+proc.communicate("writeimage_ori 0 0\nquit\n".encode())
 shutil.move(cur_dir / srcfits.name, srcfits)
 
-### make residual image
-data, header   = fits.getdata(imgfits, header=True)
+# make residual image
+data, header = fits.getdata(imgfits, header=True)
 rdata, rheader = fits.getdata(fitsfile, header=True)
 if source_num > 1:
-    data    = data[-1]
+    data = data[-1]
     resdata = rdata - data
 else:
     resdata = rdata - data
 fits.writeto(resfits, resdata, rheader, overwrite=True)
 
-### make combined image ###
+# make combined image
 f.makefigure(imgout, rdata, data, resdata)
 
-### make statistics ###
+# make statistics
 elapsed_time = time.time() - start
-statdict = {'elapsed_time': elapsed_time, 'chi^2': chi2, 'Ndata': ndata, 'reduced_chi^2': chi2 / ndata,
-            'obs_max': rdata.max(), 'obs_min': rdata.min(),
-            'mod_max': data.max(), 'mod_min': data.min(),
-            'res_max': resdata.max(), 'res_min': resdata.min()}
+statdict = {
+    "elapsed_time": elapsed_time,
+    "chi^2": chi2,
+    "Ndata": ndata,
+    "reduced_chi^2": chi2 / ndata,
+    "obs_max": rdata.max(),
+    "obs_min": rdata.min(),
+    "mod_max": data.max(),
+    "mod_min": data.min(),
+    "res_max": resdata.max(),
+    "res_min": resdata.min(),
+}
 f.makestats(statout, statdict)
 
-### qlook ###
+# qlook
 if qlook:
     shutil.rmtree(input_dir)
     shutil.rmtree(results_dir)
--- scripts/utils/functions.py
+++ scripts/utils/functions.py
@@ -3,77 +3,99 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 
-def makeinput(filename, primary_params, secondary_params, lens_models, source_models, lens_opt, source_opt):
-    fp = open(filename, 'w')
+def makeinput(
+    filename,
+    primary_params,
+    secondary_params,
+    lens_models,
+    source_models,
+    lens_opt,
+    source_opt,
+):
+    fp = open(filename, "w")
 
-    fp.write('### primary parameters ###\n')
+    fp.write("### primary parameters ###\n")
     for key, value in primary_params.items():
-        fp.write('{}\t{}\n'.format(key, value))
-    fp.write('\n')
+        fp.write("{}\t{}\n".format(key, value))
+    fp.write("\n")
 
-    fp.write('### secondary parameters ###\n')
+    fp.write("### secondary parameters ###\n")
     for key, value in secondary_params.items():
-        fp.write('{}\t{}\n'.format(key, value))
-    fp.write('\n')
+        fp.write("{}\t{}\n".format(key, value))
+    fp.write("\n")
 
-    fp.write('### startup ###\n')
-    fp.write('startup {} {} 0\n'.format(len(lens_models), len(source_models)))
-    fp.write('### lens model ###\n')
-    np.savetxt(fp, lens_models, fmt='%s')
-    fp.write('### extended source model ###\n')
-    np.savetxt(fp, source_models, fmt='%s')
-    fp.write('end_startup\n')
-    fp.write('\n')
+    fp.write("### startup ###\n")
+    fp.write("startup {} {} 0\n".format(len(lens_models), len(source_models)))
+    fp.write("### lens model ###\n")
+    np.savetxt(fp, lens_models, fmt="%s")
+    fp.write("### extended source model ###\n")
+    np.savetxt(fp, source_models, fmt="%s")
+    fp.write("end_startup\n")
+    fp.write("\n")
 
-    fp.write('### optimization ###\n')
-    fp.write('start_setopt\n')
-    fp.write('### lens opt ###\n')
-    np.savetxt(fp, lens_opt, fmt='%s')
-    fp.write('### extended source opt ###\n')
-    np.savetxt(fp, source_opt, fmt='%s')
-    fp.write('end_setopt\n')
-    fp.write('\n')
+    fp.write("### optimization ###\n")
+    fp.write("start_setopt\n")
+    fp.write("### lens opt ###\n")
+    np.savetxt(fp, lens_opt, fmt="%s")
+    fp.write("### extended source opt ###\n")
+    np.savetxt(fp, source_opt, fmt="%s")
+    fp.write("end_setopt\n")
+    fp.write("\n")
 
-    fp.write('### execute commands\n')
-    fp.write('start_command\n')
-    fp.write('\n')
+    fp.write("### execute commands\n")
+    fp.write("start_command\n")
+    fp.write("\n")
 
     fp.close()
+
 
 def makefigure(imgout, imgfits, fitsfile, resfits):
     fig, ax = plt.subplots(1, 3, figsize=(10, 4))
 
-    title = ['obs', 'mod', 'res']
-    data  = [imgfits, fitsfile, resfits]
+    title = ["obs", "mod", "res"]
+    data = [imgfits, fitsfile, resfits]
     for ax_, title_, data_ in zip(ax, title, data):
-        ax_.axis('off')
+        ax_.axis("off")
         ax_.set_title(title_)
-        mappable = ax_.imshow(data_, origin='lower', cmap='jet')
-        divider  = make_axes_locatable(ax_)
-        cax      = divider.append_axes('bottom', size='5%', pad=0.05)
-        fig.colorbar(mappable, cax=cax, orientation='horizontal')
+        mappable = ax_.imshow(data_, origin="lower", cmap="jet")
+        divider = make_axes_locatable(ax_)
+        cax = divider.append_axes("bottom", size="5%", pad=0.05)
+        fig.colorbar(mappable, cax=cax, orientation="horizontal")
 
     fig.tight_layout()
     fig.savefig(imgout)
     fig.show()
 
+
 def makestats(statout, statdict, show=True):
-    ### to be updated
-    with open(statout, 'w') as fp:
+    # to be updated
+    with open(statout, "w") as fp:
         for key, value in statdict.items():
-            fp.write(f'{key}\t{value}\n')
+            fp.write(f"{key}\t{value}\n")
 
     if show:
-        print('')
-        print('summary')
-        print('===================================')
-        print('elapsed time = {:.1e} [sec]'.format(statdict['elapsed_time']))
-        print('')
-        print('chi^2 = {:.1e}'.format(statdict['chi^2']))
-        print('Ndata = {}'.format(statdict['Ndata']))
-        print('reduced chi^2 = {:.1f}'.format(statdict['reduced_chi^2']))
-        print('')
-        print('obs min/max = {:.2e} / {:.2e}'.format(statdict['obs_min'], statdict['obs_max']))
-        print('mod min/max = {:.2e} / {:.2e}'.format(statdict['mod_min'], statdict['mod_max']))
-        print('res min/max = {:.2e} / {:.2e}'.format(statdict['res_min'], statdict['res_max']))
-        print('===================================')
+        print("")
+        print("summary")
+        print("===================================")
+        print("elapsed time = {:.1e} [sec]".format(statdict["elapsed_time"]))
+        print("")
+        print("chi^2 = {:.1e}".format(statdict["chi^2"]))
+        print("Ndata = {}".format(statdict["Ndata"]))
+        print("reduced chi^2 = {:.1f}".format(statdict["reduced_chi^2"]))
+        print("")
+        print(
+            "obs min/max = {:.2e} / {:.2e}".format(
+                statdict["obs_min"], statdict["obs_max"]
+            )
+        )
+        print(
+            "mod min/max = {:.2e} / {:.2e}".format(
+                statdict["mod_min"], statdict["mod_max"]
+            )
+        )
+        print(
+            "res min/max = {:.2e} / {:.2e}".format(
+                statdict["res_min"], statdict["res_max"]
+            )
+        )
+        print("===================================")
